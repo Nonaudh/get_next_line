73146cfbdcdf1495f25c91f1f44381478fa2c99e
Nonaudh <nonaudh@gmail.com>
2023-11-21 19:13:56 +0100

    ok

---
+++ get_next_line.h
@@ -0,0 +1,12 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+#include <unistd.h>
+#include <stdlib.h>
+
+#include <stdio.h>
+
+int	ft_strlen(const char *s);
+char	*ft_strchr(const char *src, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+
+#endif
